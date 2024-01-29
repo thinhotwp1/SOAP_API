6996a8a293fe95fe6d02926267c2d3ac34fd7a2a
ThinhLD <leduythinh20001@gmail.com>
2024-01-29 10:36:13 +0700

    update xml defined in model

--- src/main/java/org/example/demo_soap/models/Product.java
+++ src/main/java/org/example/demo_soap/models/Product.java
@@ -1,18 +1,20 @@
 package org.example.demo_soap.models;
+import jakarta.xml.bind.annotation.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@XmlRootElement(name = "product")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Product {
+    @XmlElement(name = "coder") // Defined this in xml is coder
     private Integer code;
     private String name;
     private Double price;
+    @XmlTransient // Hide this in xml
     private Date dateCreate;
-
-    public Product(Integer code, String name, Double price, Date dateCreate) {
-        this.code = code;
-        this.name = name;
-        this.price = price;
-        this.dateCreate = dateCreate;
-    }
 }
